2fea46ca838c28bcf48ccec9b053d576add1d77d
sinc <73232388+kenji176@users.noreply.github.com>
2022-05-21 17:02:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import discord
 from discord import File
 import random, string
 import asyncio
-def randomname(n):
-    randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
-    return ''.join(randlst)
 
 
 
@@ -19,19 +16,16 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-
-
-
 @client.event
 async def on_message(message):
     if "paycard1" in message.content:
         img = await loop.run_in_executor(None,card.paypay,"True",message.author.avatar_url,"sinc","100")
-        file = discord.File(img,filename=f"{randomname(10)}.png")
+        file = discord.File(img,filename="paycard.png")
         await message.channel.send(file=file)
     elif "paycard2" in message.content:
         img = await loop.run_in_executor(None,card.paypay,False,message.author.avatar_url,message.author.name,str(random.randrange(100000)))
 
-        file = discord.File(img,filename=f"{randomname(10)}.png")
+        file = discord.File(img,filename="paycard.png")
         await message.channel.send(file=file)
     else:
         pass
